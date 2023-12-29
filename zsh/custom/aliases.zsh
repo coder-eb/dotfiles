269c442fafc30ca2276c38803733898fa3e27593
coder-eb <ebranbright@gmail.com>
2023-12-29 23:26:40 +0530

    fix: change alias name

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,7 +1,7 @@
 # Fast-travel aliases
 alias gotod="$HOME/downloads"
 alias gotodot="$HOME/dotfiles"
-alias gocoding="$HOME/coding"
+alias gocode="$HOME/coding"
 
 alias gotoapi="$HOME/Matoke/flow-api"
 alias gotoui="$HOME/Matoke/flow_admin_ui"

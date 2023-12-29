f1748e97398c4444c6128ea5db67a501849b821e
coder-eb <ebranbright@gmail.com>
2023-12-29 23:08:04 +0530

    feat: shell script for new machine setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,36 @@
 #!/bin/sh
 
-# Author : coder-eb
-# Script follows here:
+# Remove .zshrc file
+zshrc_path="$HOME/.zshrc"
 
-# Remove existing files
-rm ~/.zshrc
+# Check if the file exists before attempting to remove
+if [ -e "$zshrc_path" ]; then
+    echo "Removing .zshrc file..."
+    # Attempt to remove the file
+    rm "$zshrc_path"
+    
+    # Check the exit status of the rm command
+    if [ $? -eq 0 ]; then
+        echo "Removal completed successfully."
+    else
+        exit
+    fi
+fi
 
-# Create symlinks (soft)
-ln -s zsh/.zshrc ~/.zshrc 
+target_link=$zshrc_path
+source_file="$HOME/dotfiles/zsh/.zshrc"
+
+# Check if the source file exists
+if [ -e "$source_file" ]; then
+    # Create a symbolic link
+    ln -s "$source_file" "$target_link"
+    
+    # Check the exit status of the ln command
+    if [ $? -eq 0 ]; then
+        echo "Symbolic link created successfully: $target_link -> $source_file"
+    else
+        echo "Error: Failed to create symbolic link."
+    fi
+else
+    echo "Error: Source file does not exist: $source_file"
+fi
--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,6 +1,8 @@
 # Fast-travel aliases
 alias gotod="$HOME/downloads"
+alias gotodot="$HOME/dotfiles"
 alias gocoding="$HOME/coding"
+
 alias gotoapi="$HOME/Matoke/flow-api"
 alias gotoui="$HOME/Matoke/flow_admin_ui"
 

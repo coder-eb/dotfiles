56bc5d7d7d7e26eedf85dce815874bdfcfc5c6ab
coder-eb <ebranbright@gmail.com>
2024-01-07 23:34:02 +0530

    fix: cleanup scripts folder

---
+++ scripts/zshrc_setup.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Remove .zshrc file
+zshrc_path="$HOME/.zshrc"
+
+# Check if the file exists before attempting to remove
+if [ -e "$zshrc_path" ]; then
+    echo "Removing .zshrc file..."
+    # Attempt to remove the file
+    rm "$zshrc_path"
+    
+    # Check the exit status of the rm command
+    if [ $? -eq 0 ]; then
+        echo "Removal completed successfully."
+    else
+        exit
+    fi
+fi
+
+target_link=$zshrc_path
+source_file="$HOME/dotfiles/configs/zsh/.zshrc"
+
+# Check if the source file exists
+if [ -e "$source_file" ]; then
+    # Create a symbolic link
+    ln -s "$source_file" "$target_link"
+    
+    # Check the exit status of the ln command
+    if [ $? -eq 0 ]; then
+        echo "Symbolic link created successfully: $target_link -> $source_file"
+    else
+        echo "Error: Failed to create symbolic link."
+        exit;
+    fi
+else
+    echo "Error: Source file does not exist: $source_file"
+    exit;
+fi
--- setup.sh
+++ setup.sh
@@ -1,36 +1,29 @@
 #!/bin/sh
 
-# Remove .zshrc file
-zshrc_path="$HOME/.zshrc"
-
-# Check if the file exists before attempting to remove
-if [ -e "$zshrc_path" ]; then
-    echo "Removing .zshrc file..."
-    # Attempt to remove the file
-    rm "$zshrc_path"
-    
-    # Check the exit status of the rm command
-    if [ $? -eq 0 ]; then
-        echo "Removal completed successfully."
+# Functions
+is_package_installed() {
+    package_name="$1"
+    if path=$(command -v "$package_name" 2>/dev/null); then
+        return 0
     else
-        exit
+        return 1
     fi
+}
+
+show_package_missing_warning() {
+    echo "\nwarning: '$1' is not installed"
+    echo "suggestion: use 'sudo apt install $1'"
+}
+
+if ! is_package_installed git; then
+    show_package_missing_warning git
+    exit;
 fi
 
-target_link=$zshrc_path
-source_file="$HOME/dotfiles/configs/zsh/.zshrc"
-
-# Check if the source file exists
-if [ -e "$source_file" ]; then
-    # Create a symbolic link
-    ln -s "$source_file" "$target_link"
-    
-    # Check the exit status of the ln command
-    if [ $? -eq 0 ]; then
-        echo "Symbolic link created successfully: $target_link -> $source_file"
-    else
-        echo "Error: Failed to create symbolic link."
-    fi
-else
-    echo "Error: Source file does not exist: $source_file"
+if ! is_package_installed unzip; then
+    show_package_missing_warning unzip
+    exit;
 fi
+
+sh scripts/zshrc_setup.sh
+sh scripts/nvim_setup.sh
